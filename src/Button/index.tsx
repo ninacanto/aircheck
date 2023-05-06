d1840546976cfdfc4dd1ada6b561fbde9ba237d2
Nina Fernandes da Cunha Canto <ninacanto25@gmail.com>
2023-05-06 15:53:01 -0300

    feat: test

---
+++ src/Button/index.tsx
@@ -0,0 +1,9 @@
+import style from "./button.module.css";
+import Image from "next/image";
+export function Button() {
+  return (
+    <div>
+      <Image src="../../../public/025.png" alt=""></Image>
+    </div>
+  );
+}
